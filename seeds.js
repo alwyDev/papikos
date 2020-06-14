9b4d192e4484788d373b3764ac710228f3b6173f
alwy <alwy.pro@gmail.com>
2020-06-14 12:06:13 +0000

    refactor callbacks with Async + Await

--- seeds.js
+++ seeds.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var Place = require("./models/place");
 var Comment = require("./models/comment");
 
-var data = [
+var seeds = [
 	{
 		name: "Kocheng",
 		image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/250px-Cat03.jpg",
@@ -18,48 +18,32 @@ var data = [
 		image: "https://www.kitska.com.au/wp-content/uploads/2018/09/Hairless-kitty-600x300.jpg",
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est velit egestas dui id ornare. Non enim praesent elementum facilisis leo vel fringilla est ullamcorper."
 	}
-]
+];
 
-function seedDB(){
-	//Remove all places
-	Place.remove({}, function(err){
-		if(err){
-			console.log(err);
-		}
-		console.log("removed places!");
-		Comment.remove({}, function(err){
-			if(err){
-				console.log(err);
-			}
-			console.log("removed comments!");
-		
-		//add a few places
-	data.forEach(function(seed){
-		Place.create(seed, function(err, place){
-			if(err){
-				console.log(err)
-			} else {
-				console.log("added a place");
-				//create a comment
-				Comment.create(
-					{
-						text: "This cat is cool!",
-						author: "Alwy"
-					}, function(err, comment){
-						if(err){
-							console.log(err);
-						} else {	
-							place.comments.push(comment);
-							place.save();
-							console.log("Created new comment");
-						}
-					});
+async function seedDB(){
+	try {
+		await Place.remove({});
+		console.log('place removed');
+		await Comment.remove({});
+		console.log('comment removed');
+
+		for(const seed of seeds) {
+			let place = await Place.create(seed);
+			console.log('place created');
+			let comment = Comment.create(
+				{
+					text: "This place is cool!",
+					author: "Alwy"
 				}
-			});
-		});
-	});
-});
-	//add a few comments
+			)
+			console.log('comment created');
+			place.comments.push(comment);
+			place.save();
+			console.log('comment added to place');
+		}
+	} catch(err) {
+		console.log(err);
+	}
 }
 
 module.exports = seedDB;
